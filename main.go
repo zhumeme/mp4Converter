5dfe69ebf69c40aaa2ca38e7214c1980531bf647
chy1432 <986026509@qq.com>
2023-03-29 15:41:24 +0800

    feat: h264.mp4

--- main.go
+++ main.go
@@ -63,67 +63,86 @@ func main() {
 	}
 	defer writer.Close()
 
-	for {
-		mat := gocv.NewMat()
-		if ok := vc.Read(&mat); !ok {
-			return
-		}
-		if mat.Empty() {
-			mat.Close()
-			continue
-		}
-		yuv, err := mat.ToImageYUV()
-		if err != nil {
-			panic(err)
-		}
-		yuvMat := gocv.NewMatWithSize(height, width, mat.Type())
-		gocv.CvtColor(mat, &yuvMat, gocv.ColorBGRToYUV)
+	for finished := false; !finished; {
+		finished = encode(vc, height, width, svcEncoder, writer)
+	}
+}
 
-		var srcPic C.SSourcePicture
-		srcPic.iPicWidth = C.int(width)
-		srcPic.iPicHeight = C.int(height)
-		srcPic.iColorFormat = C.int(C.videoFormatI420)
-		srcPic.uiTimeStamp = C.longlong(time.Now().UnixNano())
-		srcPic.iStride[0] = C.int(yuv.YStride)
-		srcPic.iStride[1] = C.int(yuv.CStride)
-		srcPic.iStride[2] = C.int(yuv.CStride)
-		srcPic.pData[0] = (*C.uchar)(C.CBytes(yuv.Y))
-		srcPic.pData[1] = (*C.uchar)(C.CBytes(yuv.Cb))
-		srcPic.pData[2] = (*C.uchar)(C.CBytes(yuv.Cr))
+func encode(vc *gocv.VideoCapture, height int, width int, svcEncoder *C.ISVCEncoder, writer *gocv.VideoWriter) bool {
+	mat := gocv.NewMat()
+	defer mat.Close()
+	if ok := vc.Read(&mat); !ok {
+		fmt.Println("cannot read mat")
+		return true
+	}
+	if mat.Empty() {
+		fmt.Println("mat is empty")
+		return false
+	}
+	yuv, err := mat.ToImageYUV()
+	if err != nil {
+		fmt.Println("ToImageYUV failed.", err.Error())
+		return false
+	}
+	yuvMat := gocv.NewMatWithSize(height, width, mat.Type())
+	defer yuvMat.Close()
+	gocv.CvtColor(mat, &yuvMat, gocv.ColorBGRToYUV)
 
-		var frameBSInfo C.SFrameBSInfo
-		rv = C.EncodeFrame(svcEncoder, &srcPic, &frameBSInfo)
-		if rv != 0 {
-			fmt.Println("EncodeFrame failed")
-			panic("EncodeFrame failed")
-		}
+	var srcPic C.SSourcePicture
+	srcPic.iPicWidth = C.int(width)
+	srcPic.iPicHeight = C.int(height)
+	srcPic.iColorFormat = C.int(C.videoFormatI420)
+	srcPic.uiTimeStamp = C.longlong(time.Now().UnixNano())
+	srcPic.iStride[0] = C.int(yuv.YStride)
+	srcPic.iStride[1] = C.int(yuv.CStride)
+	srcPic.iStride[2] = C.int(yuv.CStride)
+	ptrUint8, err := yuvMat.DataPtrUint8()
+	if err != nil {
+		fmt.Println("DataPtrUint8 failed.", err.Error())
+		return false
+	}
+	srcPic.pData[0] = (*C.uchar)(unsafe.Pointer(&ptrUint8[0]))
+	srcPic.pData[1] = (*C.uchar)(unsafe.Pointer(&ptrUint8[width*height]))
+	srcPic.pData[2] = (*C.uchar)(unsafe.Pointer(&ptrUint8[width*height+width*height>>2]))
+
+	var frameBSInfo C.SFrameBSInfo
+	rv := C.EncodeFrame(svcEncoder, &srcPic, &frameBSInfo)
+	if rv == VideoFrameTypeInvalid {
+		fmt.Println("EncodeFrame failed")
+		return false
+	} else if rv != VideoFrameTypeSkip {
 
 		// 使用C.SFrameBSInfo中的数据生成视频文件
 		d := make([]byte, 0)
 		for i := 0; i < int(frameBSInfo.iLayerNum); i++ {
 			layer := frameBSInfo.sLayerInfo[i]
+			var nalLen = 0
 			for j := 0; j < int(layer.iNalCount); j++ {
-				nalLen := int(*layer.pNalLengthInByte)
-				data := C.GoBytes(unsafe.Pointer(layer.pBsBuf), C.int(nalLen))
-				d = append(d, data...)
-				//imDecode, err := gocv.IMDecode(data, gocv.IMReadAnyColor)
-				//if err != nil {
-				//	panic(err)
-				//}
-				//err = writer.Write(imDecode)
-				//if err != nil {
-				//	panic(err)
-				//}
+				nalLen += int(*layer.pNalLengthInByte)
 			}
+			data := C.GoBytes(unsafe.Pointer(layer.pBsBuf), C.int(nalLen))
+			d = append(d, data...)
 		}
 
 		imDecode, err := gocv.IMDecode(d, gocv.IMReadAnyColor)
 		if err != nil {
-			panic(err)
+			fmt.Println("imDecode failed.", err.Error())
+			return false
 		}
 		err = writer.Write(imDecode)
 		if err != nil {
-			panic(err)
+			fmt.Println("writer.Write failed.", err.Error())
+			return false
 		}
 	}
+	return false
 }
+
+const (
+	VideoFrameTypeInvalid = 0x0
+	VideoFrameTypeIDR     = 0x1
+	VideoFrameTypeI       = 0x2
+	VideoFrameTypeP       = 0x3
+	VideoFrameTypeSkip    = 0x4
+	VideoFrameTypeIPMixed = 0x5
+)
